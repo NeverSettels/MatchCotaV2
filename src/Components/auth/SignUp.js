1de14f3d6246865ba923b0c8a6a0a8176ab449e3
NeverSettels <settels.eric@gmail.com>
2020-07-17 13:57:06 -0700

    added signup functionality and started to play with roles

--- src/Components/auth/SignUp.js
+++ src/Components/auth/SignUp.js
@@ -1,9 +1,56 @@
-import React from 'react'
+import React, { useState } from "react";
+import firebase from "firebase/app";
+import { message } from 'antd';
+import { useFirestore } from 'react-redux-firebase'
 
-export default function SignUp() {
+
+
+export default function Header() {
+  const [signupEmail, setSignupEmail] = useState("")
+  const [signupPassword, setSignupPassword] = useState("")
+  const [signupConfirmPassword, setSignupConfirmPassword] = useState("")
+
+  const [role, setrole] = useState("Role1")
+
+  const firestore = useFirestore()
+
+  function doSignUp() {
+    if (signupPassword === signupConfirmPassword) {
+      firebase.auth().createUserWithEmailAndPassword(signupEmail, signupPassword).then(function (data) {
+        console.log("do i get anything here=>>> ", data.user.uid)
+
+        message.success("successfully signed up!");
+
+        return firestore.collection('users').add({ userId: data.user.uid, role, liked: [] })
+      }).catch(function (error) {
+        message.error(error.message);
+      });
+    } else {
+      message.error("Passwords do not match");
+    }
+  }
+
+
   return (
     <div>
+
+      <h1>Sign up</h1>
+      <form >
+        <input onChange={e => setSignupEmail(e.target.value)} type='text' name='email' placeholder='Email' />
+        <input onChange={e => setSignupPassword(e.target.value)} type='password' name='password' placeholder='Password' />
+        <input onChange={e => setSignupConfirmPassword(e.target.value)} type='password' name='confirmPassword' placeholder='Confirm Password' />
+
+
+        <select onChange={e => setrole(e.target.value)} name="role">
+          <option value="Role1">Role1</option>
+          <option value="Role2">Role2</option>
+        </select>
+
+      </form>
+
+
 
     </div>
   )
 }
+
