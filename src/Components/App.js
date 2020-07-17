07abba3b8b60789fc8c6e3272887e280dbe7e253
NeverSettels <settels.eric@gmail.com>
2020-07-17 10:50:45 -0700

    added fire base set up, and set up initial routes'

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
-import './Components/App.css';
+import '../App.css';
+import 'antd/dist/antd.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Landing from './home/Landing';
+import Main from './profile/Main';
+import SignUp from './auth/SignUp';
+import SignIn from './auth/SignIn';
+
+
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/signup">
+          <SignUp />
+        </Route>
+        <Route path="/signin">
+          <SignIn />
+        </Route>
+        <Route path="/profile">
+          <Main />
+        </Route>
+        <Route path="/">
+          <Landing />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
