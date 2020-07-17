1e8108eae41932fe4033621bd8a5253f4ef7b4a3
NeverSettels <settels.eric@gmail.com>
2020-07-17 15:41:33 -0700

    Add sign in

--- src/Components/auth/SignIn.js
+++ src/Components/auth/SignIn.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import firebase from "firebase/app";
+import { message } from 'antd';
 
 export default function SignIn() {
+
+  const [signinEmail, setSigninEmail] = useState("")
+  const [signinPassword, setSigninPassword] = useState("")
+
+  function doSignIn() {
+    firebase.auth().signInWithEmailAndPassword(signinEmail, signinPassword).then(function () {
+      message.success("Successfully signed in!");
+    }).catch(function (error) {
+      message.error(error.message);
+    });
+  }
   return (
     <div>
-
+      <h1>Sign In</h1>
+      <form >
+        <input onChange={e => setSigninEmail(e.target.value)} type='text' name='email' placeholder='Email' />
+        <input onChange={e => setSigninPassword(e.target.value)} type='password' name='password' placeholder='Password' />
+        <button onClick={doSignIn} >Sign In</button>
+      </form>
     </div>
   )
 }
