f0c7feec238dc0e09c1a2c96a08335f4d9e36016
NeverSettels <settels.eric@gmail.com>
2020-07-28 11:40:30 -0700

    working on like view still

--- src/Components/profile/pets/PetCard.js
+++ src/Components/profile/pets/PetCard.js
@@ -1,52 +1,68 @@
-import React,{useState, useEffect} from 'react'
-import firebase from 'firebase/app';
+import React, { useState, useEffect, useContext } from "react";
+import { MyContext } from "../../../context";
+import firebase from "firebase/app";
+import { Carousel } from "antd";
 export default function PetCard(props) {
-    const [urls, seturls] = useState([])
+  const [urls, seturls] = useState([]);
+  const context = useContext(MyContext);
+  const { pet, like, dislike } = props;
+  let storage = firebase.storage();
+  const getUrls = () => {
+    for (let i = 0; i < 3; i++) {
+      let refUrl = `src/public/${pet.poster}/${pet.imageId}/image${i}`;
+      let tempreference = storage.ref(refUrl);
+      tempreference
+        .getDownloadURL()
+        .then((url) => {
+          console.log("something =>", url);
+          seturls((prev) => [...prev, url]);
+        })
+        .catch(function (error) {
+          console.log(error);
+          // A full list of error codes is available at
+          // https://firebase.google.com/docs/storage/web/handle-errors
+          switch (error.code) {
+            case "storage/object-not-found":
+              // File doesn't exist
+              break;
 
- const {pet}= props
- let storage = firebase.storage();
-const getUrls=()=>{
-    for(let i=0; i<3; i++){
-        let refUrl = `src/public/${pet.poster}/${pet.imageId}/image${i}`
-        let tempreference = storage.ref(refUrl);
-        tempreference.getDownloadURL().then(url=>{
-            console.log("something =>", url)
-            seturls(prev=>[...prev,url])
-        }).catch(function(error) {
-            console.log(error)
-            // A full list of error codes is available at
-            // https://firebase.google.com/docs/storage/web/handle-errors
-            switch (error.code) {
-              case 'storage/object-not-found':
-                // File doesn't exist
-                break;
-          
-              case 'storage/unauthorized':
-                // User doesn't have permission to access the object
-                break;
-          
-              case 'storage/canceled':
-                // User canceled the upload
-                break;
-          
-              case 'storage/unknown':
-                // Unknown error occurred, inspect the server response
-                break;
-                default:
-                    break;
-            }
-          })
+            case "storage/unauthorized":
+              // User doesn't have permission to access the object
+              break;
+
+            case "storage/canceled":
+              // User canceled the upload
+              break;
+
+            case "storage/unknown":
+              // Unknown error occurred, inspect the server response
+              break;
+            default:
+              break;
+          }
+        });
     }
-}
+  };
 
-useEffect(() => {
-    getUrls()
-}, [])
+  useEffect(() => {
+    getUrls();
+  }, []);
 
-    return (
-        <div key={pet.id}>
-            {pet.petName}
-            {urls.map((url, i) => <img key={i} src={url} alt={`image${i}`}/>)}
-        </div>
-    )
+  return (
+    <div key={pet.id}>
+      <Carousel autoplay>
+        {urls.map((url, i) => (
+          <img key={i} src={url} alt={`image${i}`} />
+        ))}
+      </Carousel>
+      <h2>{pet.petName}</h2>
+      {pet.poster !== context.state.user.uid ? (
+        <>
+          <button onClick={like}>like</button>
+        </>
+      ) : (
+        ""
+      )}
+    </div>
+  );
 }
